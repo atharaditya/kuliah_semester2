681f928b921794575a51ff0cffad893ab409fc2f
atharaditya <athanala13@gmail.com>
2024-11-14 11:16:36 +0700

    Delete Hello directory

--- Hello/src/Coba_coba/Test.java
+++
@@ -1,8 +0,0 @@
-package Coba_coba;
-public class Test {
-
-    public static void main(String[] args){
-
-        System.out.println("nama saya atha");
-    } 
-}
--- Hello/src/Jobsheet1/HelloJava.java
+++
@@ -1,6 +0,0 @@
-package Jobsheet1;
-public class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-    }
-}
